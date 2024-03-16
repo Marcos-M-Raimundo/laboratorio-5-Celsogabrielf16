84babdcdb0fbe19e7bb69227a101b386608b63c1
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-16 20:20:38 -0300

    Criação dos setters da classe Item

--- src/Item.java
+++ src/Item.java
@@ -3,7 +3,7 @@ public class Item {
     private int bonusPoder;
     private int valor;
     private String tipo;
-    
+
     public Item(String nome, int bonusPoder, int valor, String tipo) {
         this.nome = nome;
         this.bonusPoder = bonusPoder;
@@ -15,15 +15,31 @@ public class Item {
         return this.nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public int getBonusPoder() {
         return this.bonusPoder;
+    }
+
+    public void setBonusPoder(int bonusPoder) {
+        this.bonusPoder = bonusPoder;
     }
 
     public int getValor() {
         return this.valor;
     }
+    
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
         
     public String getTipo() {
         return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
