11d155f807607acfe54e94a5cb90817284b820ca
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-16 20:39:22 -0300

    Criação dos setters da classe Monstro

--- src/Monstro.java
+++ src/Monstro.java
@@ -15,15 +15,31 @@ public class Monstro {
         return this.nome;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public int getPoder() {
         return this.poder;
+    }
+
+    public void setPoder(int poder) {
+        this.poder = poder;
     }
 
     public int getQuantidadeDeTesouros() {
         return this.quantidadeDeTesouros;
     }
 
+    public void setQuantidadeDeTesouros(int quantidadeDeTesouros) {
+        this.quantidadeDeTesouros = quantidadeDeTesouros;
+    }
+
     public int getNiveisPerdidos() {
         return this.niveisPerdidos;
+    }
+
+    public void setNiveisPerdidos(int niveisPerdidos) {
+        this.niveisPerdidos = niveisPerdidos;
     }
 }
