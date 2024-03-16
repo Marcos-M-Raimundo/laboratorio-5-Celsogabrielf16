fc5cb0dfea0c2ba68b2683feb9485c8845c0765e
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-16 20:19:14 -0300

    Criação dos getters da classe Item

--- src/Item.java
+++ src/Item.java
@@ -4,11 +4,26 @@ public class Item {
     private int valor;
     private String tipo;
     
-    // Construtor que recebe todas as propriedades
     public Item(String nome, int bonusPoder, int valor, String tipo) {
         this.nome = nome;
         this.bonusPoder = bonusPoder;
         this.valor = valor;
         this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getBonusPoder() {
+        return this.bonusPoder;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+        
+    public String getTipo() {
+        return this.tipo;
     }
 }
