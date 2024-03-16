3953e40ffb7dd69abf125998fad74f8bc05f93f6
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-16 18:53:25 -0300

    Criação dos setters da classe Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -16,37 +16,73 @@ public class Jogador {
     public String getNome() {
         return this.nome;
     }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public int getNivel() {
         return this.nivel;
+    }
+    
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
     }
 
     public String getRaca() {
         return this.raca;
     }
     
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+        
     public String getClasse() {
         return this.classe;
     }
     
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+        
     public String getItemCabeca() {
         return this.itemCabeca;
     }
     
+    public void setItemCabeca(String itemCabeca) {
+        this.itemCabeca = itemCabeca;
+    }
+        
     public String getItemCorpo() {
         return this.itemCorpo;
     }
     
+    public void setItemCorpo(String itemCorpo) {
+        this.itemCorpo = itemCorpo;
+    }
+        
     public String getItemMaoDireita() {
         return this.itemMaoDireita;
     }
     
-    public String getItemMaoesquerda() {
+    public void setItemMaoDireita(String itemMaoDireita) {
+        this.itemMaoDireita = itemMaoDireita;
+    }
+        
+    public String getItemMaoEsquerda() {
         return this.itemMaoEsquerda;
     }
     
+    public void setItemMaoEsquerda(String itemMaoEsquerda) {
+        this.itemMaoEsquerda = itemMaoEsquerda;
+    }
+        
     public String getItemPe() {
         return this.itemPe;
     }
     
+    public void setItemPe(String itemPe) {
+        this.itemPe = itemPe;
+    }
+        
 }
