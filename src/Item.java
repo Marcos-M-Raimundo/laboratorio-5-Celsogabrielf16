2e39d88d75c4d75932bf0e19c01871fcca4ccb1c
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-16 20:17:44 -0300

    Declaração dos atributos da classe Item

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,14 @@
 public class Item {
-    public Item() {
-        System.out.println("Classe item criada!");
+    private String nome;
+    private int bonusPoder;
+    private int valor;
+    private String tipo;
+    
+    // Construtor que recebe todas as propriedades
+    public Item(String nome, int bonusPoder, int valor, String tipo) {
+        this.nome = nome;
+        this.bonusPoder = bonusPoder;
+        this.valor = valor;
+        this.tipo = tipo;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Jogador jogador1 = new Jogador("Celso", 16, "Humano", "Unicamper", "Capacete de Tartaruga", "Armadura de Netherite", "Espada de Dima", "Escudo", "Bota de Ferro");
         Jogador jogador2 = new Jogador("Gabriel Prado");
-        Item item = new Item();
+        Item item = new Item("Armadura de Netherite", 10, 5, "itemCorpo");
         Monstro monstro = new Monstro();
 
         System.out.println(jogador1.toString());
