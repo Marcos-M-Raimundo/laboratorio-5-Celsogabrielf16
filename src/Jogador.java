b776f6485d3ac2bc41502fbafff00661e5815041
Celso Gabriel Prado <celsogabrielf@gmail.com>
2024-03-16 18:40:08 -0300

    Declaração dos atributos da classe Jogador

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,4 +1,14 @@
 public class Jogador {
+    private String nome;
+    private int nivel;
+    private String raca;
+    private String classe;
+    private String itemCabeca;
+    private String itemCorpo;
+    private String itemMaoDireita;
+    private String itemMaoEsquerda;
+    private String itemPe;
+
     public Jogador() {
         System.out.println("Classe jogador criada!");
     }
