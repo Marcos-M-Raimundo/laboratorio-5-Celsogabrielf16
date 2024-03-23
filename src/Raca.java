e8287190c801b2d9100ad5a094e632e5eb4ce37e
Celsogabrielf16 <celsogabrielf@gmail.com>
2024-03-23 12:34:11 -0300

    Declaração dos atributos da classe Raca

--- src/Raca.java
+++ src/Raca.java
@@ -1,3 +1,11 @@
 public class Raca {
-    public Raca() {}
+    private final String descricao; 
+    private String nomeDaRaca; 
+    private final int minimoPraFugir; 
+
+    public Raca(String descricao, String nomeDaRaca, int minimoPraFugir) {
+        this.descricao = descricao;
+        this.nomeDaRaca = nomeDaRaca;
+        this.minimoPraFugir = minimoPraFugir;
+    }
 }
