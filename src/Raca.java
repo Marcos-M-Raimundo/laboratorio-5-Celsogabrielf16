1b827722a9e09653e9b9505c57f225d8d672079a
Celsogabrielf16 <celsogabrielf@gmail.com>
2024-03-23 12:39:12 -0300

    Criação dos getters da classe Raca

--- src/Raca.java
+++ src/Raca.java
@@ -1,11 +1,23 @@
 public class Raca {
-    private final String descricao; 
-    private String nomeDaRaca; 
-    private final int minimoPraFugir; 
+    private final String descricao;
+    private String nomeDaRaca;
+    private final int minimoPraFugir;
 
     public Raca(String descricao, String nomeDaRaca, int minimoPraFugir) {
         this.descricao = descricao;
         this.nomeDaRaca = nomeDaRaca;
         this.minimoPraFugir = minimoPraFugir;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public String getNomeDaRaca() {
+        return this.nomeDaRaca;
+    }
+
+    public int getMinimoPraFugir() {
+        return this.minimoPraFugir;
     }
 }
