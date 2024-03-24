c3be606806d28558da953a7cc2dbcbf806f71baf
Celsogabrielf16 <celsogabrielf@gmail.com>
2024-03-24 10:59:17 -0300

    Criação da classe herdeira Guerreiro

---
+++ src/Guerreiro.java
@@ -0,0 +1,14 @@
+public class Guerreiro extends Classe {
+    private Item itemCabeca;
+    private Item itemCorpo;
+    
+    public Guerreiro(String descricao) {
+        super(descricao);
+    }
+
+    public Guerreiro(String descricao, Item itemMaoDireita, Item itemMaoEsquerda, Item itemPe, Item itemCabeca, Item itemCorpo) {
+        super(descricao, itemMaoDireita, itemMaoEsquerda, itemPe);
+        this.itemCabeca = itemCabeca;
+        this.itemCorpo = itemCorpo;
+    }
+}
