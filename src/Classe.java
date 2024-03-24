0c38e6ae8eb8396940999df1cdf019cff6293110
Celsogabrielf16 <celsogabrielf@gmail.com>
2024-03-23 21:17:25 -0300

    Declaração dos atributos da classe Classe

--- src/Classe.java
+++ src/Classe.java
@@ -1,3 +1,13 @@
 public class Classe {
-    public Classe() {}
+    private final String descricao;
+    private Item itemMaoDireita;
+    private Item itemMaoEsquerda;
+    private Item itemPe;
+
+    public Classe(String descricao, Item itemMaoDireita, Item itemMaoEsquerda, Item itemPe) {
+        this.descricao = descricao;
+        this.itemMaoDireita = itemMaoDireita;
+        this.itemMaoEsquerda = itemMaoEsquerda;
+        this.itemPe = itemPe;
+    }
 }
