6d59acf4f11e8e0c1d07701e89f81d3200ab0d67
Celsogabrielf16 <celsogabrielf@gmail.com>
2024-04-06 18:10:05 -0300

    Criação da classe Masmorra

---
+++ src/Masmorra.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Masmorra {
+    private List<Monstro> monstros;
+    private BauDeTesouros tesouros;
+
+    public Masmorra(List<Monstro> monstros, BauDeTesouros tesouros) {
+        this.monstros = monstros;
+        this.tesouros = tesouros;
+    }
+}
