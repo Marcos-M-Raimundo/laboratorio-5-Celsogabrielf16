9c9feeb00bda9b00dc030fe237d69fe6cbc163a6
Celsogabrielf16 <celsogabrielf@gmail.com>
2024-04-06 12:06:00 -0300

    Refatoração da classe Inventario

--- src/Inventario.java
+++ src/Inventario.java
@@ -2,13 +2,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Inventario {
-    private final int limiteInventario;
+    private final int limiteDeItens;
     private List<Item> inventario;
 
-    public Inventario(int limiteInventario) {
+    public Inventario(int limiteDeItens) {
+        this.limiteDeItens = limiteDeItens;
         // Criamos um inventario vazio
         this.inventario = new LinkedList<>();
-        this.limiteInventario = limiteInventario;
     }
 
     // Método padrao chamado quando imprimimos a classe inventario
@@ -19,8 +19,8 @@ public class Inventario {
         } else {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append("Inventario { ")
-                .append("Limite inventario: ").append(this.limiteInventario)
-                .append(", Inventario: {");
+                .append("Limite inventario: ").append(this.limiteDeItens)
+                .append(", Inventario: { ");
             for (Item itemInventario : this.inventario) {
                 stringBuilder.append(itemInventario);
             }
@@ -30,16 +30,7 @@ public class Inventario {
     }
 
     public boolean adicionarItem(Item item) {
-        return this.inventario.size() < this.limiteInventario ? this.inventario.add(item) : false;
-    }
-
-    public void removerItem(String nomeItem) {
-        for (Item itemInventario : this.inventario) {
-            if (itemInventario.getNome().equals(nomeItem)) {
-                this.inventario.remove(itemInventario);
-                break;
-            }
-        }
+        return this.inventario.size() < this.limiteDeItens ? this.inventario.add(item) : false;
     }
 
     public Item acessarItem(String nomeItem) {
@@ -54,13 +45,33 @@ public class Inventario {
         return itemEncontrado;
     }
 
-    public void listarInventario() {
+    public void removerItem(String nomeItem) {
+        for (Item itemInventario : this.inventario) {
+            if (itemInventario.getNome().equals(nomeItem)) {
+                this.inventario.remove(itemInventario);
+                break;
+            }
+        }
+    }
+
+    public void removerItem(List<String> listaNomesItens) {
+        for (Item itemInventario : this.inventario) {
+            for (String nomeItem : listaNomesItens) {
+                if (itemInventario.getNome().equals(nomeItem)) {
+                    this.inventario.remove(itemInventario);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void listarItens() {
         for (Item itemInventario : this.inventario) {
             System.out.println(itemInventario);
         }
     }
 
-    public int getLimiteInventario() {
-        return this.limiteInventario;
+    public int getLimiteDeItens() {
+        return this.limiteDeItens;
     }
 }
